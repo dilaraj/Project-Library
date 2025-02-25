5a8c39fdc716822519171272d0e4816cb5ea96e0
Dilara Ajaj <106478364+dilaraj@users.noreply.github.com>
2025-02-25 08:45:32 +0000

    Implementing functionality to store added books in an array and display them on screen'

--- script.js
+++ script.js
@@ -13,5 +13,30 @@ function addBookToLibrary(title, author, numOfPages, isRead) {
 }
 
 addBookToLibrary("Harry Potter", "J.K Rowling", 500, true);
+addBookToLibrary("Babel, or the Necessity of Violence", "R. F. Kuang", 560, false);
+addBookToLibrary("Sherlock Holmes", "William Steig", 603, false);
 console.log(myLibrary);
 
+
+function displayLibrary() {
+    const libraryContainer = document.querySelector(".library-container");
+
+    libraryContainer.innerHTML = "";
+
+    myLibrary.forEach((book, index) => {
+        const bookDiv = document.createElement("div");
+        bookDiv.classList.add("book");
+
+        bookDiv.innerHTML = `
+        <div class="title">Title: ${book.title}</div>
+        <div class="author">Author ${book.author}</div>
+        <div class="numOfPages">Number of Pages: ${book.numOfPages}</div>
+        <div class="isRead">Is Read? ${book.isRead}</div>
+        <br>
+        `;
+
+        libraryContainer.appendChild(bookDiv);
+    });
+}
+
+displayLibrary();
